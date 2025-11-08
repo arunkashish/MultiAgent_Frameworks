0a296a401d25364f154af06a0d128b2ee2238a56
Arun Kumar <arunkumarecekrct@gmail.com>
2025-11-08 10:48:06 +0800

    lst_agn

--- Agno/AgnoTeams/team_in_agno.py
+++ Agno/AgnoTeams/team_in_agno.py
@@ -13,27 +13,25 @@ from agno.utils.pprint import pprint_run_response
 import os
 
 # Set your API key (replace YOUR_API_KEY with your actual key)
-#os.environ["GOOGLE_API_KEY"] = "your-api-key-here"
-os.environ["OPENAI_API_KEY"] = (
-    "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"
-)
+# os.environ["GOOGLE_API_KEY"] = "your-api-key-here"
+os.environ["OPENAI_API_KEY"] = ""
 
 # Verify it’s set
 print("GOOGLE_API_KEY set successfully!")
 
 # Create specialized agents
 news_agent = Agent(
-    id="news-agent", # Important
-    name="News Agent", # Important
-    role="Get the latest news and provide summaries", # Important
-    tools=[DuckDuckGoTools()]
+    id="news-agent",  # Important
+    name="News Agent",  # Important
+    role="Get the latest news and provide summaries",  # Important
+    tools=[DuckDuckGoTools()],
 )
 
 weather_agent = Agent(
-    id="weather-agent", # Important
-    name="Weather Agent", # Important
-    role="Get weather information and forecasts",# Important
-    tools=[DuckDuckGoTools()]
+    id="weather-agent",  # Important
+    name="Weather Agent",  # Important
+    role="Get weather information and forecasts",  # Important
+    tools=[DuckDuckGoTools()],
 )
 
 # Create the team
@@ -41,12 +39,11 @@ team = Team(
     name="News and Weather Team",
     members=[news_agent, weather_agent],
     model=OpenAIChat(id="gpt-4o"),
-    instructions="Coordinate with team members to provide comprehensive information. " \
-    "Delegate tasks based on the user's request."
+    instructions="Coordinate with team members to provide comprehensive information. "
+    "Delegate tasks based on the user's request.",
 )
 
 team.print_response("What's the latest news and weather in Tokyo?", stream=True)
-
 
 
 # # Run team and return the response as a variable
